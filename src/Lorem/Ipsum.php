fb367ac50f2a382141a9383745892a33549512ae
Isaac Suttell <isaac@e-mc2.com>
2013-11-01 17:51:46 -0700

    added basic comments

--- src/Lorem/Ipsum.php
+++ src/Lorem/Ipsum.php
@@ -10,6 +10,12 @@ class Ipsum {
 	 */
 	private static $words = array("a", "ac", "accumsan", "adipiscing", "aenean", "aliquam", "aliquet", "amet", "ante", "arcu", "at", "auctor", "augue", "bibendum", "blandit", "commodo", "condimentum", "congue", "consectetur", "consequat", "convallis", "cras", "cubilia", "curabitur", "curae", "cursus", "dapibus", "diam", "dictum", "dignissim", "dolor", "donec", "dui", "duis", "egestas", "eget", "eleifend", "elementum", "elit", "enim", "erat", "eros", "est", "et", "etiam", "eu", "euismod", "facilisis", "fames", "faucibus", "felis", "fermentum", "feugiat", "fringilla", "fusce", "gravida", "habitant", "hendrerit", "I", "iaculis", "id", "imperdiet", "in", "integer", "interdum", "ipsum", "justo", "lacinia", "lacus", "laoreet", "lectus", "leo", "libero", "ligula", "lobortis", "lorem", "luctus", "maecenas", "magna", "malesuada", "massa", "mattis", "mauris", "metus", "mi", "molestie", "mollis", "morbi", "nam", "nec", "neque", "netus", "nibh", "nisi", "nisl", "non", "nulla", "nullam", "nunc", "odio", "orci", "ornare", "pellentesque", "pharetra", "phasellus", "placerat", "porta", "porttitor", "posuere", "potenti", "praesent", "pretium", "primis", "proin", "pulvinar", "purus", "quam", "quis", "quisque", "rhoncus", "risus", "rutrum", "sagittis", "sapien", "scelerisque", "sed", "sem", "semper", "senectus", "sit", "sodales", "sollicitudin", "suscipit", "suspendisse", "tellus", "tempor", "tempus", "tincidunt", "tortor", "tristique", "turpis", "ullamcorper", "ultrices", "ultricies", "urna", "ut", "varius", "vehicula", "vel", "velit", "venenatis", "vestibulum", "vitae", "vivamus", "viverra", "volutpat", "vulputate");
 
+
+	/**
+	 * Generates by default by random parahraphs
+	 * @param  integer $count the number of paragraphs to create
+	 * @return string
+	 */
 	public static function paragraphs($count = 5)
 	{
 		$paragraphs = '';
@@ -20,10 +26,17 @@ class Ipsum {
 		return trim($paragraphs);
 	}
 
+	/**
+	 * Create by default five sentices
+	 * @param  integer $count the number of sentences to create
+	 * @return string
+	 */
 	public static function sentences($count = 5)
 	{
 		$sentences = '';
 		for($i = 0; $i < $count; $i++ ) {
+
+			//Randomly add commas to the sentence in logical places
 			$rand = mt_rand(0, 3);
 			if($rand === 0){
 				$sentence = static::words(mt_rand(3, 8));
@@ -38,7 +51,11 @@ class Ipsum {
 			} else {
 				$sentence = static::words(mt_rand(5, 20));
 			}
+
+			//Capitlize the first word
 			$sentences .= ucfirst(trim($sentence));
+
+			//Ocassionally use a semi-colon
 			switch(mt_rand(0, 10)){
 				case 0:
 					$sentences .= '; ';
@@ -48,10 +65,18 @@ class Ipsum {
 			}
 		}
 		$sentences = trim($sentences);
+
+		//Make sure a semicolon is the last character
 		if(substr($sentences, -1) === ';') $sentences = substr($sentences, 0, -1) . '.';
+
 		return $sentences;
 	}
 
+	/**
+	 * Generate by default 5 random words
+	 * @param  integer $count the number of words to create
+	 * @return string
+	 */
 	public static function words($count = 5)
 	{
 		$words = '';
@@ -61,6 +86,11 @@ class Ipsum {
 		return trim($words);
 	}
 
+	/**
+	 * Generate by default five capitilized words
+	 * @param  integer $count the number of words to create
+	 * @return string
+	 */
 	public static function title($count = 5)
 	{
 		$words = static::words($count);
