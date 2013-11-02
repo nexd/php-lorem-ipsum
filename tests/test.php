80cc6f1e8dfa1a0be247d6f1699eea71af411cda
Isaac Suttell <isaac@e-mc2.com>
2013-11-01 17:51:37 -0700

    included all methods

--- tests/test.php
+++ tests/test.php
@@ -4,4 +4,22 @@ require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Compos
 
 use Lorem\Ipsum;
 
+echo "*** Paragraphs ***".PHP_EOL;
 echo Ipsum::paragraphs();
+
+echo PHP_EOL . PHP_EOL;
+
+echo "*** Sentences ***".PHP_EOL;
+echo Ipsum::sentences();
+
+echo PHP_EOL . PHP_EOL;
+
+echo "*** Words ***".PHP_EOL;
+echo Ipsum::words();
+
+echo PHP_EOL . PHP_EOL;
+
+echo "*** Title ***".PHP_EOL;
+echo Ipsum::title();
+
+echo PHP_EOL . PHP_EOL;
